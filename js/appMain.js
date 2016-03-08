98a47d709a036f3a6ed56cf9aaaab08b3742fe0d
lockan <lockan@gmail.com>
2016-03-07 16:58:14 -0800

    WIP - testing dynamic HTML injection

--- js/appMain.js
+++ js/appMain.js
@@ -1,11 +1,21 @@
-var appMain = angular.module("appMain", []);
+var appMain = angular.module("appMain", ['ngSanitize']);
 
-appMain.controller("mainController", function ($scope, $http) {
+appMain.controller("mainController", function ($scope, $http, $sce) {
 
-	$scope.pageheader = "Page Header"
-	$scope.test = "This is a test"
-	$scope.title = "Sample title"
-	$scope.subtitle = "Subtitle"
-	$scope.description = "This is some content text that I wrote"
+	$scope.pageheader = "Page Header";
+	$scope.test = "This is a test";
+	$scope.title = "Sample title";
+	$scope.subtitle = "Subtitle";
+	$scope.description = "This is some content text that I wrote";
+	
+	var testdata 
+	$http.get('/htmltest.html').then(function(response) { 
+		testdata = response.data;
+	});
+	
+	
+	$scope.renderbox = function() { 
+		return $sce.trustAsHtml(testdata);
+	};
 	
 });
